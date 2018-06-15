e08672799b3ae3abeb887a6d43524937b5bc4c63
Umut Acar <umutacar@gmail.com>
2018-06-15 11:59:12 -0400

    WIP: problem sets

--- dex_mlx/pervasives/os_utils.py
+++ dex_mlx/pervasives/os_utils.py
@@ -3,13 +3,27 @@ import os
 from pervasives.syntax import *
 
 # Common file extensions
-DEX_EXTENSION = '.dex'
-DIL_EXTENSION = '.dil'
-HTML_EXTENSION = '.html'
-LATEX_EXTENSION = '.tex'
-MLX_EXTENSION = '.mlx'
-PDF_EXTENSION = '.pdf'
-XML_EXTENSION = '.xml'
+DEX_EXTENSION = 'dex'
+DIL_EXTENSION = 'dil'
+HTML_EXTENSION = 'html'
+LATEX_EXTENSION = 'tex'
+MLX_EXTENSION = 'mlx'
+PDF_EXTENSION = 'pdf'
+XML_EXTENSION = 'xml'
+
+# Common file derivatives
+BEAMED = '_beamed'
+DECORATED = 'decd'
+ELABORATED = 'eld'
+# File name creation
+ELABORATED_0 = 'ed_0'
+ELABORATED_1 = 'ed_1'
+LOADED = 'loaded'
+PARSED = 'parsed'
+STRICT = 'strict'
+
+TMP_DIR = '/tmp/'
+DESKTOP_DIR = '~/Desktop/'
 
 
 # Common file prefixes
@@ -22,16 +36,6 @@ SOLUTION_EXTENSION = ':solution'
 TITLE_EXTENSION = ':title'
 INFO_EXTENSION = ":info"
 
-# File name creation
-BEAMED = '_beamed'
-ELABORATED_0 = '_ed_0'
-ELABORATED_1 = '_ed_1'
-ELABORATED = '_ed'
-LOADED = '_loaded'
-PARSED = '_parsed'
-
-TMP_DIR = '/tmp/'
-DESKTOP_DIR = '~/Desktop/'
 ######################################################################
 ## DEBUG HANDLING
 
@@ -81,10 +85,32 @@ def cp_file_to_desktop (filename):
 def cp_file_to (filename1, filename2):
   command = 'cp ' + SPACE + filename1 + SPACE + filename2
   run_command_no_check(command)
-
+  
 def mv_file_to (filename1, filename2):
   command = 'mv ' + SPACE + filename1 + SPACE + filename2
   run_command_no_check(command)    
   
-## END: Run commands
+
+
+  ## END: Run commands
 ######################################################################
+
+# mk a new filename out the infilename
+# name.ext turns into name_derivative.extension
+
+def mk_file_name (infile_name, derivative, extension):
+  (infile_name_first, infile_ext) = infile_name.split (PERIOD) 
+  outfile_name = infile_name_first + UNDERSCORE + derivative + PERIOD + extension
+  return outfile_name
+
+def mk_file_name_ext (infile_name, extension):
+  (infile_name_first, infile_ext) = infile_name.split (PERIOD) 
+  outfile_name = infile_name_first + PERIOD + extension
+  return outfile_name
+
+def deep_cp_file_to (infile_name, outfile_name):
+  infile = open(infile_name, 'rb')
+  outfile = open(outfile_name, 'w')
+
+  data = infile.read ()
+  outfile.write (data)
