b929e80aa3575bddb60e845a864a0a9418ddba7d
Charles Yuan <charlesyuan314@gmail.com>
2018-09-30 22:54:20 -0400

    new lexer

--- dex_dil_ocaml/src/top.ml
+++ dex_dil_ocaml/src/top.ml
@@ -1,19 +1,17 @@
 open Core
 open Lexer
 open Lexing
+open MenhirLib
 
 let filename = Sys.argv.(1)
 
 let main () =
-  let inx = In_channel.create filename in
-  let lexbuf = Lexing.from_channel inx in
-  lexbuf.lex_curr_p <- { lexbuf.lex_curr_p with pos_fname = filename };
+  let lines = In_channel.read_lines filename in
   try
-    print_endline (Pp.pp_blocks (Parser.blocks Lexer.read lexbuf))
+    let parser = MenhirLib.Convert.Simplified.traditional2revised Parser.blocks in
+    print_endline (Pp.pp_blocks (parser (Lexer.lex lines)))
   with
   | Parser.Error ->
-      Printf.eprintf "At offset %d: syntax error.\n%!" (Lexing.lexeme_start lexbuf);
-      Printf.eprintf "Unexpected: %s\n" (Lexing.lexeme lexbuf);
-  In_channel.close inx
+      Printf.eprintf "Syntax error."
 
   let _ = main ()
