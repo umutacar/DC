d205680099480200414a7417d15d70154f7d8ba4
Umut Acar <umutacar@gmail.com>
2018-12-09 22:54:04 -0500

    WIP: parser with ast

--- dex_ocaml/ast.ml
+++ dex_ocaml/ast.ml
@@ -15,3 +15,26 @@ and section =
 and block = 
   | Block_Group of group
   | Block_Atom of atom
+
+let atomToString a as Atom(topt, hb, ab, he) = 
+  match topt
+  | None => hb ^ ab ^ he
+  | Some t => hb ^ "title:" ^ t ^ ab ^ he
+
+let groupToString g as Group(topt, hb, ats, he) = 
+  let atoms = List.map atomToString ats in
+    match topt
+    | None => hb ^ ats ^ he
+    | Some t => hb ^ "title:" ^ t ^ ats ^ he
+
+let rec astToString ast = 
+  match ast 
+  | Chapter (t, h, bs, ss) =>
+    let blocks = List.map toString bs in 
+    let sections = List.map toString ss in
+      h ^ blocks ^ ss
+  | Section (t, h, bs) =>
+    let blocks = List.map toString bs in
+      h ^ blocks
+  | Block_Group g => groupToString g
+  | Block_Atom a => atomToString a
