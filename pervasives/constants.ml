3aceb83f1f3bc401627877b1a1ec8983a5bc14f7
Umut A. Acar <umut@cs.cmu.edu>
2018-12-19 12:53:00 -0500

    adedd diderot constants

---
+++ pervasives/constants.ml
@@ -0,0 +1,53 @@
+(**********************************************************************
+ ** Diderot Constants 
+ **********************************************************************)
+
+(* Error conditions that can occur as tokens *)
+const_not_provided = "...NOT.PROVIDED."
+const_no_answer = const_not_provided + "ANSWER..." 
+const_no_answers = const_not_provided + "ANSWERS..." 
+const_no_checkpoint = const_not_provided + "CHECKPOINT..." 
+const_no_explain = const_not_provided + "EXPLANATION..." 
+const_no_hint = const_not_provided + "HINT..." 
+const_no_intro = const_not_provided + "INTRO..." 
+const_no_info = const_not_provided + "INFO..."
+const_no_parents = const_not_provided + "PARENTS..." 
+const_no_points = "0"  # Still has to be a number
+const_no_prompt = const_not_provided + "PROMPT..." 
+
+const_no_solution = const_not_provided + "UNKOWN.SOLUTION..." 
+const_no_label = const_not_provided + "LABEL..." 
+const_no_no = "0"  # has to be a number still 
+const_no_rank = "0"  # has to be a number still 
+const_no_title = const_not_provided + "TITLE..." 
+const_no_topics = const_not_provided + "TOPICS..."
+const_no_unique = "0"  # has to be a number still 
+
+
+(* Label prefixes *)
+const_LABEL_PREFIX_ANSWER = "answer"
+const_LABEL_PREFIX_ASSIGNMENT = "assignment"
+const_LABEL_PREFIX_ASSTPROBLEM = "asstproblem"
+const_LABEL_PREFIX_ATOM = "at"
+const_LABEL_PREFIX_BOOK = "book"
+const_LABEL_PREFIX_CHAPTER = "ch"
+const_LABEL_PREFIX_CHOICE = "choice"
+const_LABEL_PREFIX_COURSE = "course"
+const_LABEL_PREFIX_GRAM = "gram"
+const_LABEL_PREFIX_GROUP = "gr"
+const_LABEL_PREFIX_CHECKPOINT = "checkpoint"
+const_LABEL_PREFIX_PROBLEM = "problem"
+const_LABEL_PREFIX_PROBLEM_GROUP = "problem_group"
+const_LABEL_PREFIX_PROBLEM_SET = "problem_set"
+const_LABEL_PREFIX_PROBLEM_FR = "problem_fr"
+const_LABEL_PREFIX_PROBLEM_MA = "problem_ma"
+const_LABEL_PREFIX_PROBLEM_MC = "problem_mc"
+const_LABEL_PREFIX_QUESTION_FR = "question_fr"
+const_LABEL_PREFIX_QUESTION_MA = "question_ma"
+const_LABEL_PREFIX_QUESTION_MC = "question_mc"
+const_LABEL_PREFIX_SECTION = "sec"
+const_LABEL_PREFIX_SELECT = "select"
+const_LABEL_PREFIX_SEMESTER = "semester"
+const_LABEL_PREFIX_SUBSECTION = "subsec"
+const_LABEL_PREFIX_SUBSUBSECTION = "subsubsec"
+
--- tex/keywords.ml
+++ tex/keywords.ml
@@ -1,84 +1,16 @@
-## Error conditions that can occur as tokens
-kw_not_provided = '...NOT.PROVIDED.'
-kw_no_answer = KW_NOT_PROVIDED + 'ANSWER...' 
-kw_no_answers = KW_NOT_PROVIDED + 'ANSWERS...' 
-kw_no_checkpoint = KW_NOT_PROVIDED + 'CHECKPOINT...' 
-kw_no_explain = KW_NOT_PROVIDED + 'EXPLANATION...' 
-kw_no_hint = KW_NOT_PROVIDED + 'HINT...' 
-kw_no_intro = KW_NOT_PROVIDED + 'INTRO...' 
-kw_no_info = KW_NOT_PROVIDED + 'INFO...'
-kw_no_parents = KW_NOT_PROVIDED + 'PARENTS...' 
-kw_no_points = '0'  # Still has to be a number
-kw_points_correct = '1'  # Still has to be a number
-kw_no_prompt = KW_NOT_PROVIDED + 'PROMPT...' 
+(**********************************************************************
+ ** Diderot Keywords
+ **********************************************************************)
 
-kw_no_solution = KW_NOT_PROVIDED + 'UNKOWN.SOLUTION...' 
-kw_no_label = KW_NOT_PROVIDED + 'LABEL...' 
-kw_no_title = KW_NOT_PROVIDED + 'TITLE...' 
-kw_no_topics = KW_NOT_PROVIDED + 'TOPICS...'
-kw_no_unique = KW_NOT_PROVIDED + 'UNIQUE...' 
-
-# These are handled specially
-kw_no_unique = '0'  # has to be a number still 
-kw_no_no = '0'  # has to be a number still 
-kw_no_rank = '0'  # has to be a number still 
-
-# commands, start with BACKSLASH
-COM_BEGIN = BACKSLASH + r'begin'
-COM_END = BACKSLASH + r'end'
-COM_LABEL = BACKSLASH + r'label'
-COM_NO =  BACKSLASH + r'no'
-COM_PARENT = BACKSLASH + r'parent'
-COM_PARENTS = BACKSLASH + r'parents'
-COM_UNIQUE =  BACKSLASH + r'unique'
-
-## Keywords, don't start with a backshlash
-KW_ATOM_TITLE_PREFIX = 'Atom'
-KW_CHAPTER_TITLE_PREFIX = 'Chapter'
-KW_QUESTION_TITLE_PREFIX = 'Question'
-KW_CHECKPOINT_TITLE_PREFIX = 'Checkpoint'
-KW_SECTION_TITLE_PREFIX = 'Section'
-KW_SUBSECTION_TITLE_PREFIX = 'Subsection'
-
-# Label prefixes
-KW_LABEL_PREFIX_ANSWER = 'answer'
-KW_LABEL_PREFIX_ASSIGNMENT = 'assignment'
-KW_LABEL_PREFIX_ASSTPROBLEM = 'asstproblem'
-KW_LABEL_PREFIX_ATOM = 'at'
-KW_LABEL_PREFIX_BOOK = 'book'
-KW_LABEL_PREFIX_CHAPTER = 'ch'
-KW_LABEL_PREFIX_CHOICE = 'choice'
-KW_LABEL_PREFIX_COURSE = 'course'
-KW_LABEL_PREFIX_GRAM = 'gram'
-KW_LABEL_PREFIX_GROUP = 'gr'
-KW_LABEL_PREFIX_CHECKPOINT = 'checkpoint'
-KW_LABEL_PREFIX_PROBLEM = 'problem'
-KW_LABEL_PREFIX_PROBLEM_GROUP = 'problem_group'
-KW_LABEL_PREFIX_PROBLEM_SET = 'problem_set'
-KW_LABEL_PREFIX_PROBLEM_FR = 'problem_fr'
-KW_LABEL_PREFIX_PROBLEM_MA = 'problem_ma'
-KW_LABEL_PREFIX_PROBLEM_MC = 'problem_mc'
-KW_LABEL_PREFIX_QUESTION_FR = 'question_fr'
-KW_LABEL_PREFIX_QUESTION_MA = 'question_ma'
-KW_LABEL_PREFIX_QUESTION_MC = 'question_mc'
-KW_LABEL_PREFIX_SECTION = 'sec'
-KW_LABEL_PREFIX_SELECT = 'select'
-KW_LABEL_PREFIX_SEMESTER = 'semester'
-KW_LABEL_PREFIX_SUBSECTION = 'subsec'
-KW_LABEL_PREFIX_SUBSUBSECTION = 'subsubsec'
-
-## Keywords, don't start with a backshlash
-KW_BOOK_NO = r'bookno'
-KW_COURSE_NO = r'courseno'
-KW_CHAPTER_NO = r'chapterno'
-KW_FALSE = r'False'
-KW_SECTION_NO = r'sectionno'
-KW_ATOM_NO = r'atomno'
-KW_QUESTION_NO = r'problemno'
-KW_CHECKPOINT_NO = r'checkpointno'
-KW_PARENT = r'parent'
-KW_PROMPT = r'prompt'
+KW_BEGIN = BACKSLASH + r'begin'
+KW_END = BACKSLASH + r'end'
+KW_LABEL = BACKSLASH + r'label'
+KW_NO =  BACKSLASH + r'no'
+KW_PARENT = BACKSLASH + r'parent'
+KW_PARENTS = BACKSLASH + r'parents'
 KW_POINTS = r'points'
-KW_TRUE = r'True'
-KW_SUBSECTION_NO = r'subsectionno'
+KW_PROMPT = r'prompt'
+KW_UNIQUE =  BACKSLASH + r'unique'
+
+
 
