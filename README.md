e5c89aa93140d13dce081043c80eb6d45ebddc6a
Umut Acar <umutacar@gmail.com>
2019-01-01 21:37:35 -0500

    made tex2xml more usable

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
-# Files
+# Building the library
+Running  `make` should make two executables
+1. tex2tex.native
+2. tex2xml.native
+
+
+# Contents
 The new ocaml implementation of MeTaL consists only of two directories
  
-xml/
-tex/
+1. `xml/`
+2. `tex/`
 
 The rest of the directories are python stuff and should be depracated and removed overe time.
--- tex/tex2xml.ml
+++ tex/tex2xml.ml
@@ -4,34 +4,31 @@ open Lexing
 
 
 let default_tmp_dir = "/tmp/"
-let default_preamble_file = "/Users/umut/15210/algobook/latex_preamble/preamble.tex"
-          
-let mk_translator () = 
-  let preamble = In_channel.read_all default_preamble_file in
+
+let mk_translator preamble_filename = 
+  let preamble = In_channel.read_all preamble_filename in
     Tex2html.mk_translator (default_tmp_dir, preamble) 
 
 let main () =
 	let args = Sys.argv in
-    if Array.length args >= 2 then
+    if Array.length args == 4 then
       let filename = Sys.argv.(1) in
+      let preamble_filename = Sys.argv.(2) in
 			let ic = In_channel.create filename in
       let chapter_xml =   
   			try 
           let lexbuf = Lexing.from_channel ic in
 	  	  	let ast_chapter = Parser.chapter Lexer.token lexbuf in
-          let tex2html = mk_translator () in
+          let tex2html = mk_translator preamble_filename in
           let chapter_xml = Ast.chapterToXml tex2html ast_chapter in
             printf "Parsed successfully chapter.\n";
             chapter_xml
         with End_of_file -> exit 0
       in 
-        if Array.length args = 3 then    
-          let outfile = Sys.argv.(2) in
-            Out_channel.write_all outfile ~data:chapter_xml 
-        else
-          printf "Parsing complete."
+      let outfile = Sys.argv.(3) in
+         Out_channel.write_all outfile ~data:chapter_xml 
     else
-      printf "Usage: main <input latex file> [<output xml file>]\n";;			
+      printf "Usage: main <input latex file> <input preamble file> [<output xml file>]\n";;			
 					
 let _ = main ()
 
