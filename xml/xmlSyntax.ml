59a620c4f7d5f9a6006f7d3edbe822a1b26871b5
Umut Acar <umutacar@gmail.com>
2019-03-12 20:46:24 -0400

    items and ilists must have label fields

--- xml/xmlSyntax.ml
+++ xml/xmlSyntax.ml
@@ -254,14 +254,16 @@ let mk_block_generic_with_kind name kind fields =
     | Some r ->  b ^ C.newline ^ r ^ C.newline ^ e ^ C.newline
 
 let mk_item ~body_src ~body_xml = 
+  let label_xml = mk_label_opt None in
   let body_xml = mk_body body_xml in
   let body_src = mk_body_src body_src in
-    mk_block_generic item [body_xml; body_src] 
+    mk_block_generic item [label_xml; body_xml; body_src] 
 
 let mk_ilist ~kind ~topt ~t_xml_opt ~body = 
   let title_xml = mk_title_opt t_xml_opt in
   let title_src = mk_title_src_opt topt in
-    mk_block_generic_with_kind ilist kind [title_xml; title_src; body]
+  let label_xml = mk_label_opt None in
+    mk_block_generic_with_kind ilist kind [title_xml; title_src; label_xml; body]
 
 let mk_atom ~kind ~topt ~t_xml_opt ~lopt ~body_src ~body_xml ~ilist = 
   let title_xml = mk_title_opt t_xml_opt in
