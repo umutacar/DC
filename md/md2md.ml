59f7db6eefac97d1215a7853f77813d85fde8520
Umut Acar <umutacar@gmail.com>
2019-07-25 21:40:01 -0400

    added missing files

---
+++ md/md2md.ml
@@ -0,0 +1,93 @@
+(* texel.ml 
+ * Elaborator tool for LaTex.
+ *) 
+open Core
+open Lexing
+open Utils
+
+
+module Ast = Il_ast
+module Lexer = Md_lexer
+module Parser = Md_parser
+
+let verbose = ref false
+let do_groups = ref false
+let do_inline = ref false
+let in_file = ref None
+let out_file = ref None
+
+(* Set string argument *)
+let set_str_arg r v = r := Some v
+
+(* Get value of string argument *)
+let get_str_arg r v =
+  match !r with 
+  | None -> (printf "Fatal Error"; exit 1)
+  | Some s -> s
+
+(* Stub. *)
+let handle_parser_error () = 
+  printf ("Parse Error\n.")
+
+
+let md2ast infile = 
+	let ic = In_channel.create infile in
+   	try 
+      let lexbuf = Lexing.from_channel ic in
+	    let ast = Parser.top Lexer.lexer lexbuf in
+			match ast with 
+			| None -> (printf "Parse Error."; exit 1)
+			| Some ast -> ast
+    with End_of_file -> exit 0
+
+
+let md2md do_inline infile = 
+  (* Preprocess *)
+  let infile_inlined = 
+        if do_inline then
+          Preprocessor.inline_file infile
+        else 
+          infile
+  in
+  (* Make AST *)
+	let ast = md2ast infile_inlined in
+
+  (* Translate to Tex *)
+  let md = Ast.to_md ast in
+    md
+
+					
+let main () = 
+  let spec = [
+(*              ("-inline", Arg.Set do_inline, "Inline markdown input directives; default is false."); *)
+              ("-o", Arg.String (set_str_arg out_file), "Sets output file")
+             ]
+  in 
+
+  let take_infile_name anon = 
+    match !in_file with 
+    | None -> in_file := Some anon
+    | Some _ -> (printf "Warning: multiple input files specified, taking first.\n")
+  in
+
+  let usage_msg = "Reads in a LaTex file, parses and builds its Abstract Syntax Tree, and writes it out. \n Usage: md2md <markdown file>.\n Options available:" 
+  in
+  let _  = Arg.parse spec take_infile_name usage_msg in
+  let in_file_name =  
+    match !in_file with 
+    | None -> (printf "Error: Missing input Latex file! \n%s" (Arg.usage_string spec usage_msg); exit 1)
+    | Some x -> x
+  in
+  let _ = printf "Executing command: md2md %s\n" in_file_name in
+  let result = md2md !do_inline in_file_name  in
+    match !out_file with 
+    | None -> 
+        printf "Successfully translater chapter to TeX:\n %s\n" result 
+    | Some outfile ->
+      let _ = Out_channel.write_all outfile ~data:result in
+      let _ = printf "Successfully translated chapter. Output in %s\n" outfile in
+        ()
+
+let _ = main ()
+
+
---
+++ md/md_syntax.ml
@@ -0,0 +1,273 @@
+(**********************************************************************
+ ** md/md_syntax.ml
+ ** TODO: This needs cleanup.  There is  a lot of unused definitions here.
+
+ **********************************************************************)
+open Core
+open Utils
+
+module Il = Il_syntax
+
+exception Md_syntax_error
+
+(* Turn off prints *)
+let d_printf args = 
+    ifprintf stdout args
+let d_printf_strlist x y = 
+	()
+
+let newline = "\n"
+let space = " "
+let colon = ":"
+let correct_choice_indicator = "*"
+let label_seperator = colon
+let label_nestor = colon ^ colon
+
+
+let pattern_hs = "[ \t]*"  
+let pattern_ws = "[ \t\r\n]*"  
+let pattern_begin env =
+	"\\\\begin" ^ pattern_hs ^ "{" ^ 
+	pattern_hs ^ env ^ pattern_hs ^ "}"
+
+let pattern_end env =
+	"\\\\end" ^ pattern_hs ^ "{" ^ 
+	pattern_hs ^ env ^ pattern_hs ^ "}"
+
+
+
+let pattern_label = "\\\\label" ^ pattern_ws ^ "{" ^ pattern_ws ^ 
+	                  "[^} \r\t\n]*" ^ pattern_ws ^ "}"
+let pattern_caption = "\\\\caption"
+
+(* BEGIN: Keywords *)
+let com_depend = "\\depend"
+let com_explain = "\\explain"
+let com_hint = "\\help"
+let com_label = "\\label"
+let com_notes = "\\notes"
+let com_rubric = "\\rubric"
+let com_solution = "\\sol"
+
+
+let kw_title = "title"
+
+let kw_chapter = "#"
+let kw_section = "##"
+let kw_subsection = "###"
+let kw_subsubsection = "####"
+let kw_paragraph = "#####"
+
+let kw_cluster = "cluster"
+let kw_problem_cluster = "mproblem"
+let kw_flex = "flex"
+
+let kw_code = "code"
+let kw_algorithm = "algorithm"
+let kw_assumption = "assumption"
+let kw_corollary = "corollary"
+let kw_costspec = "costspec"
+let kw_datastr = "datastr"
+let kw_datatype = "datatype"
+let kw_definition = "definition"
+let kw_example = "example"
+let kw_figure = "figure"
+let kw_exercise = "exercise"
+let kw_hint = "hint"
+let kw_important = "important"
+let kw_lemma = "lemma"
+let kw_note = "note"
+let kw_gram = "gram"
+let kw_preamble = "preamble"
+let kw_problem = "problem"
+let kw_proof = "proof"
+let kw_proposition = "proposition"
+let kw_remark = "remark"
+let kw_reminder = "reminder"
+let kw_slide = "slide"
+let kw_solution = "solution"
+let kw_syntax = "syntax"
+let kw_table = "table"
+let kw_task = "task"
+let kw_teachask = "teachask"
+let kw_teachnote = "teachnote"
+let kw_theorem = "theorem"
+
+let kw_one_choice = "\\onechoice"
+let kw_any_choice = "\\anychoice"
+let kw_free_response = "\\freeresponse"
+let kw_short_answer = "\\shortanswer"
+
+let kw_choice = "\\choice"
+let kw_choice_correct = "\\choice*"
+let kw_part = "\\part"
+
+(* END: Keywords *)
+
+(* BEGIN: lstlisting arguments *)
+
+let firstline = "firstline"
+let language = "language"
+let none = "none"
+let numbers = "numbers"
+
+(* END: lstlisting arguments *)
+
+(*
+	 (printf "FATAL ERROR: unknown kind encountered kind = %s.\n" kind;
+   exit Error_code.labeling_error_unknown_atom)
+*)
+(* END: label prefixes *)
+(* Utilities *)
+let kw_of_segment s = 
+  if s = Il.segment_chapter then
+		kw_chapter
+  else if s = Il.segment_section then
+		kw_section
+  else if s = Il.segment_subsection then
+		kw_subsection
+  else if s = Il.segment_subsubsection then
+		kw_subsubsection
+  else if s = Il.segment_paragraph then
+		kw_paragraph
+  else
+    raise Md_syntax_error 
+
+
+let mk_arg arg = 
+  "{" ^ arg ^ "}"
+
+let mk_opt_arg x = 
+  "[" ^ x ^ "]"
+
+let mk_point_val popt = 
+  match popt with 
+  |  None -> ""
+  |  Some pts -> mk_opt_arg pts
+
+let mk_label lopt = 
+  match lopt with 
+  |  None -> ""
+  |  Some label -> ""
+
+let mk_depend dopt = 
+  let heading = com_depend in
+	match dopt with 
+  |  None -> ""
+  |  Some ls -> ""
+
+let mk_explain e = 
+  match e with 
+  |  None -> ""
+  |  Some x -> ""
+
+let mk_hint e = 
+  match e with 
+  |  None -> ""
+  |  Some x -> ""
+
+let mk_notes e = 
+  match e with 
+  |  None -> ""
+  |  Some x -> ""
+
+let mk_rubric e = 
+  match e with 
+  |  None -> ""
+  |  Some x -> ""
+
+let mk_title topt = 
+  match topt with 
+  |  None -> ""
+  |  Some t -> t
+
+let mk_command kind p = 
+  ""
+
+let mk_segment_header kind p t = 
+  let kw =  kw_of_segment kind in
+    kw ^ space ^ t ^ newline
+
+let mk_begin name p t = 
+  ""
+
+let mk_end kind = 
+  ""
+
+
+let mk_hint_opt hint_opt = 
+  let heading = com_hint in
+  match hint_opt with 
+  |  None -> ""
+  |  Some x -> ""
+
+let mk_exp_opt exp_opt = 
+  let heading = com_explain in
+  match exp_opt with 
+  |  None -> ""
+  |  Some x -> ""
+
+let refsol_opt refsol_opt = 
+  let heading = com_solution in
+  match refsol_opt with 
+  |  None -> ""
+  |  Some x -> ""
+
+let rubric_opt rubric_opt = 
+  let heading = com_rubric in
+  match rubric_opt with 
+  |  None -> ""
+  |  Some x ->  ""
+
+(**********************************************************************
+ ** BEGIN: DEPRACATED
+ **********************************************************************)
+(** TODO EXTEND THIS TO SOMETHING LIKE
+ ** IS_ATOMIC AND compare the kind against the atom keyword.
+ ** IF NO MATCH, NOT ATOM.
+ **)
+(* Given contents string
+ * check if contents has the "atomic" form
+ * \begin{env} body \end{env}.
+ * Atomic means that env does not occur within body.
+ * (We don't allow for nesting of atoms.)
+ * Return env and body if match occurs, None otherwise.
+ *)
+(*
+let take_single_env contents = 
+  match find_all_env contents with 
+	| None -> None
+	| Some (all_envs, _) ->      
+			let uniques = uniques_of_list all_envs in
+			let check env =
+				let pb = pattern_begin env in
+				let pe = pattern_end env in
+				let mb = str_match_one_first pb contents in
+				match mb with 
+				| None -> None
+				| Some (_, rest) ->
+						let me = str_match_last pe rest in
+						match me with 
+						| None -> None
+						| Some (se, body) ->
+								let _ = d_printf "tex_syntax.take_env_body matched body: \n %s" body in
+								Some (env, body)
+			in
+			let rec check_all uniques = 
+				match uniques with 
+				| [ ] -> None 
+				| h::t -> 
+						begin
+							match check h with 
+							| None -> check_all t
+							| Some (env, body) -> Some (env, body)
+						end
+			in
+			check_all uniques
+
+*)
+(**********************************************************************
+ ** END: DEPRACATED
+ **********************************************************************)
+
+
