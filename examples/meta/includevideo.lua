b818e4bb2fc8192d45702a13181cc25bc8768668
Umut Acar <umutacar@gmail.com>
2019-08-04 00:15:21 +0300

    cite source

--- examples/meta/includevideo.lua
+++ examples/meta/includevideo.lua
@@ -1,7 +1,13 @@
--- This script promotes the language attribute of a pandoc
--- codeBlock to a class.  
--- This should allows using custom languages with 
--- Kate (XML) specifications.
+-- This script includes the contents of a code block as raw html
+-- if it starts with %%%html tag
+-- This is meant for including a video.
+-- Basen on this post: https://superuser.com/questions/1393318/allow-raw-html-in-latex-code-with-pandoc
+-- \newcommand{\includevideo}[1]{
+-- \begin{verbatim}
+-- %%%html
+-- <iframe width="560" height="315" src="https://www.youtube.com/embed/eOrAa0itnHM" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
+-- </iframe>
+-- \end{verbatim}
 
 function CodeBlock(cb)
   local rawHtml = cb.text:match('^%s*%%%%%%html\n(.*)')
