f24c10e7153a9928d4edf2c367cfe2bb1f995ce7
Jennifer Lee <jenniferlee@DESKTOP-5G1LEAV.wv.cc.cmu.edu>
2019-09-19 18:00:25 -0400

    Revert "tex_parser.py : incorporate re library"
    This reverts commit 9b7636fd5a62f4f80d944c906906dc456efa7322.

--- tex_parser.py
+++ tex_parser.py
@@ -1,7 +1,5 @@
 from parse import *
 import Queue
-import sys
-import re
 
 # constants
 BEGIN = "\\begin{"
@@ -9,7 +7,6 @@ END = "\\end{"
 BEGIN_LEN = len("\\begin{")
 END_LEN = len("\\end{")
 GROUPING_ARGS = ['cluster', 'flex']
-MATH_ARGS = ['math', 'equation', 'align', 'array', 'table', 'tabular']
 
 # given a dictionary of key phrases mapped to their labels, write them to file_name
 def write_to_file(dictionary, file_name):
@@ -23,97 +20,76 @@ def write_to_file(dictionary, file_name):
 	output.close()
 
 # find an atom word with stirng
-# def find_atom_word(string):
-# 	index = string.find(BEGIN)
-# 	begin_str = string[index + BEGIN_LEN:]
-# 	atom_word = begin_str[:begin_str.find("}")]
-# 	return atom_word
+def find_atom_word(string):
+	index = string.find(BEGIN)
+	begin_str = string[index + BEGIN_LEN:]
+	atom_word = begin_str[:begin_str.find("}")]
+	return atom_word
 
 # return a list of key phrases from string
-# def find_keyphrases(string):
-# 	defn_index = string.find("\\defn{")
-# 	if (defn_index == -1):
-# 		return list()
-# 	end_index = string.find("}", defn_index)
-# 	new_string = string[end_index+1:]
-# 	subarray = find_keyphrases(new_string)
-# 	subarray.append(string[defn_index+len("\\defn{"):end_index])
-# 	return subarray
+def find_keyphrases(string):
+	defn_index = string.find("\\defn{")
+	if (defn_index == -1):
+		return list()
+	end_index = string.find("}", defn_index)
+	new_string = string[end_index+1:]
+	subarray = find_keyphrases(new_string)
+	subarray.append(string[defn_index+len("\\defn{"):end_index])
+	return subarray
 
 # Returns true if latex string contains an atom and false otws
-# def contains_atom(tex_str):
-# 	begin_start_index = tex_str.find(BEGIN)
-# 	if (begin_start_index == -1):
-# 		return False
-# 	prefix_length = begin_start_index + BEGIN_LEN
-# 	begin_finish_index = tex_str.find('}', prefix_length)
-# 	command = tex_str[prefix_length:begin_finish_index]
+def contains_atom(tex_str):
+	begin_start_index = tex_str.find(BEGIN)
+	if (begin_start_index == -1):
+		return False
+	prefix_length = begin_start_index + BEGIN_LEN
+	begin_finish_index = tex_str.find('}', prefix_length)
+	command = tex_str[prefix_length:begin_finish_index]
 
-# 	end_start_index = tex_str.find("\\end{" + command + "}")
-# 	if (end_start_index == -1):
-# 		return False
-# 	return True
+	end_start_index = tex_str.find("\\end{" + command + "}")
+	if (end_start_index == -1):
+		return False
+	return True
 
 # Returns (start_index_of_atom, end_index_of_atom) of tex_str if 
 # atom exists and false otws
-# def find_atom(tex_str):
-# 	begin_start_index = tex_str.find(BEGIN)
-# 	if (begin_start_index == -1):
-# 		return (-1, -1)
-# 	prefix_length = begin_start_index + BEGIN_LEN
-# 	begin_finish_index = tex_str.find('}', prefix_length)
-# 	command = tex_str[prefix_length:begin_finish_index]
-# 	end_start_index = tex_str.find("\\end{" + command + "}")
-# 	if (end_start_index == -1):
-# 		return (-1, -1)
-# 	end_finish_index = end_start_index + len("\\end{" + command + "}")
-# 	return (begin_start_index, end_finish_index)
+def find_atom(tex_str):
+	begin_start_index = tex_str.find(BEGIN)
+	if (begin_start_index == -1):
+		return (-1, -1)
+	prefix_length = begin_start_index + BEGIN_LEN
+	begin_finish_index = tex_str.find('}', prefix_length)
+	command = tex_str[prefix_length:begin_finish_index]
+	end_start_index = tex_str.find("\\end{" + command + "}")
+	if (end_start_index == -1):
+		return (-1, -1)
+	end_finish_index = end_start_index + len("\\end{" + command + "}")
+	return (begin_start_index, end_finish_index)
 
 def parser(tex_str):
 	label_to_atom = {}
-	defn_list = re.findall(r'\\defn{[ \w-]*}', tex_str)
-	def_list = re.findall(r'\\def{[ \w-]*}', tex_str)
-	defn_keyphrase = list(set([defn[6:-1] for defn in defn_list] + [defn[5:-1] for defn in defn_list]))
-	begin_index = 0
-	begin_definition = re.search(r'\\begin{definition*}', tex_str)
-	while (tex_str and begin_definition):
-		end_definition = re.search(r'\\end{definition*}', tex_str)
-		atom = tex_str[begin_definition.start(): end_definition.end()]
-		def_list, defn_list = re.findall(r'\\def{[ \w-]*}', atom), re.findall(r'\\defn{[ \w-]*}', atom)
-		definitions = [defn[6:-1] for defn in defn_list] + [defn[5:-1] for defn in def_list]
-		label = re.findall(r'\\label{.*}', atom)
-		print(atom, definitions, label)
-		tex_str = tex_str[end_definition.end():]
-		begin_definition = re.search(r'\\begin{definition*}', tex_str)
-		if (len(label) != 1):
-			print("Error: there exists multiple or no labels for an atom")
-			break
-		for defn in definitions:
-			label_to_atom[defn] = label[0]
-	return label_to_atom
-
-	# while (len(tex_str)):
-	# 	token_arg = find_atom_word(tex_str)
-	# 	(begin_index, end_index) = find_atom(tex_str)
+	while (len(tex_str)):
+		token_arg = find_atom_word(tex_str)
+		(begin_index, end_index) = find_atom(tex_str)
 		
-	# 	if (begin_index == -1 and end_index == -1):
-	# 		tex_str = ""
-	# 	# elif (not (token_arg.strip() in GROUPING_ARGS) and token_arg != "example"):
-	# 	elif (token_arg.strip() in ["definition", "theorem"]):
-	# 		atom = tex_str[begin_index:end_index]
-	# 		tex_str = tex_str[end_index:]
-	# 		label_index = atom.find("\\label{")
+		if (begin_index == -1 and end_index == -1):
+			tex_str = ""
+		# elif (not (token_arg.strip() in GROUPING_ARGS) and token_arg != "example"):
+		elif (token_arg.strip() in ["definition", "theorem"]):
+			atom = tex_str[begin_index:end_index]
+			tex_str = tex_str[end_index:]
+			label_index = atom.find("\\label{")
 
-	# 		keyphrases = find_keyphrases(atom)
-	# 		if (label_index != -1 and keyphrases != []):
-	# 			end_label_index = atom.find("}", label_index)
-	# 			label = atom[label_index + BEGIN_LEN:end_label_index]
-	# 			for keyphrase in keyphrases:
-	# 				label_to_atom[keyphrase] = label
-	# 	else:
-	# 		tmp = tex_str.strip().split('\n')
-	# 		tex_str = "\n".join(tmp[1:])
-	# return label_to_atom
+			keyphrases = find_keyphrases(atom)
+			if (label_index != -1 and keyphrases != []):
+				end_label_index = atom.find("}", label_index)
+				label = atom[label_index + BEGIN_LEN:end_label_index]
+				for keyphrase in keyphrases:
+					label_to_atom[keyphrase] = label
+		else:
+			tmp = tex_str.strip().split('\n')
+			tex_str = "\n".join(tmp[1:])
+	return label_to_atom
 
 # return true if end of string[:index] contains an argument to a command
 def is_argument(string, index):
@@ -149,16 +125,13 @@ def is_math_expression(string, index):
 		return True
 	elif (string.count("\[", 0, index) - string.count("\]", 0, index) == 1):
 		return True
-	for arg in MATH_ARGS:
-		begin_arg = re.search(string)
 	return False
 
 # given string and map of keyphrases to labels, identify the key pheases
 # and insert the labels as necessary
 def insert_label(string, def_to_Label):
 	lowercase_copy = string.lower()
-	str_iref = ""
-	for definition, label in def_to_Label.items():
+	for definition, label in def_to_Label.items()[0:1]:
 		prev_end = 0
 		# only use lowercase_copy for finding occurrence case-insensitive
 		index = lowercase_copy.find(definition.lower())
@@ -178,11 +151,7 @@ def insert_label(string, def_to_Label):
 	return str_iref
 
 if __name__=='__main__':
-	if (len(sys.argv) > 1):
-		tex_file = sys.argv[1]
-		f = open(tex_file, "r")
-		tex_str = f.read()
-		result = parser(tex_str)
-		new_latex = insert_label(tex_str, result)
-	else:
-		print("no argument was provided. Please provide a latex file you'd like to parse")
+	f = open("Chapter_01_Strings_and_Encodings.tex", "r")
+	tex_str = f.read()
+	result = parser(tex_str)
+	new_latex = insert_label(tex_str, result)
